95481fe4a44579e4e7116ce50ab5064bc4c416fd
Kotaro Sakamoto <sakamoto@besna.institute>
2021-06-13 19:32:01 -0700

    updated

--- docs/focus.js
+++ docs/focus.js
@@ -5,35 +5,39 @@ const colorEdges = () => {
   const edges = [];
   const nodes = [];
   const emphasized = "emphasized"
+  const neighbour = "neighbour"
   dot['description'].forEach( element => {
     element.map( line => regexEdge.exec(line.trim()) )
-      .filter( matcher => matcher != null)
+      .filter( matcher => matcher != null )
       .forEach( matcher => edges.push(matcher) )
-    element.map( line => regexNode.exec(line.trim()))
-      .filter( matcher => matcher != null)
+    element.map( line => regexNode.exec(line.trim()) )
+      .filter( matcher => matcher != null )
       .forEach( matcher => {
-        while ((m = regexNodeName.exec(matcher[0])) != null) {
+        while ( (m = regexNodeName.exec(matcher[0])) != null )
           nodes.push(m[1])
-        }
-      }
-    )
-  })
+      } )
+  } )
   const nodeElement = (node) => document.getElementById("node" + (nodes.indexOf(node) + 1))
+  const findNode = (node, i, j) =>
+    edges.filter( matcher => matcher[i] == node )
+      .map( matcher => nodeElement(matcher[j]) )
   const coloring = (node) => {
-    nodeElement(node)
-      .classList.add(emphasized)
+    findNode(node, 1, 2).concat(findNode(node, 2, 1))
+      .forEach( n => n.classList.add(neighbour) )
+    nodeElement(node).classList.add(emphasized)
     edges.filter( matcher => matcher[1] == node || matcher[2] == node )
       .map( t => edges.indexOf(t) )
       .map( index => index + 1 )
       .map( i => "edge" + i )
       .map( id => document.getElementById(id) )
-      .forEach( edge => edge.classList.add(emphasized))
+      .forEach( edge => edge.classList.add(emphasized) )
   }
   const refresh = () => {
-    Array.from(document.getElementsByClassName(emphasized))
-      .forEach( element =>
-        element.classList.remove(emphasized)
-      )
+    const remove = (className) =>
+      Array.from(document.getElementsByClassName(className))
+        .forEach( element => element.classList.remove(className) )
+    remove(emphasized)
+    remove(neighbour)
   }
   nodes.forEach( node =>
     nodeElement(node).addEventListener("mouseover", () => {
